a76f37fa54d441c7b95eaac7abecab74797c1f87
sashahx <dxethx@gmail.com>
2023-12-14 14:58:48 +0200

    add get_order method, make some changes to create_order_api_spec test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 from flask import Flask, g, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import IntegerField, StringField
 from wtforms.validators import DataRequired
 
 from src.adapters.database_adapters import DatabaseAdapter
@@ -13,9 +13,13 @@ app.config["WTF_CSRF_ENABLED"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///takehome_db"
 
 
-class OrderForm(FlaskForm):  # type: ignore[misc]
+class CreateOrderForm(FlaskForm):  # type: ignore[misc]
     name = StringField("name", [DataRequired()])
     address = StringField("address", [DataRequired()])
+
+
+class GetOrderForm(FlaskForm):  # type: ignore[misc]
+    order_id = IntegerField("order_id", [DataRequired()])
 
 
 def get_database_connection(database_uri: str) -> DatabaseAdapter:
@@ -26,13 +30,25 @@ def get_database_connection(database_uri: str) -> DatabaseAdapter:
 
 @app.route("/create", methods=["POST"])  # type: ignore[misc]
 def create_order() -> Tuple[str, int]:
-    form = OrderForm()
+    form = CreateOrderForm()
     if form.validate():
         order = Order(None, form.name.data, form.address.data)
         get_database_connection(app.config["SQLALCHEMY_DATABASE_URI"]).create_order(
             order
         )
         return render_template("success.html"), 200
+    else:
+        return render_template("failed.html"), 400
+
+
+@app.route("/get", methods=["GET"])  # type: ignore[misc]
+def get_order() -> Tuple[str, int]:
+    form = GetOrderForm()
+    if form.validate():
+        result = get_database_connection(
+            app.config["SQLALCHEMY_DATABASE_URI"]
+        ).get_order(form.order_id.data)
+        return render_template("order.html", data=result), 200
     else:
         return render_template("failed.html"), 400
 
--- spec/create_order_api_spec.py
+++ spec/create_order_api_spec.py
@@ -1,3 +1,4 @@
+import xml.etree.ElementTree as ET
 from unittest.mock import Mock, patch
 
 from expects import equal, expect
@@ -7,9 +8,17 @@ from app import app
 from src.adapters.database_adapters import DatabaseAdapter
 
 
-class MockOrderForm(Mock):
+class MockCreateOrderForm(Mock):
     name = type("DataAttributeHolder", (), {"data": "John Doe"})
     address = type("DataAttributeHolder", (), {"data": "idk some address"})
+
+    @staticmethod
+    def validate() -> bool:
+        return True
+
+
+class MockGetOrderForm(Mock):
+    order_id = type("DataAttributeHolder", (), {"data": 1})
 
     @staticmethod
     def validate() -> bool:
@@ -30,11 +39,14 @@ with description("create order endpoint") as self:
         expect(response.status_code).to(equal(400))
 
     with it("tries to create an order and should succeed"):
-        with patch("app.OrderForm", MockOrderForm, create=True):
+        with patch("app.CreateOrderForm", MockCreateOrderForm, create=True):
             response = self.client.post("/create")
         expect(response.status_code).to(equal(200))
 
     with it("tries to get an order created at previous step"):
-        order = self.database.get_order(1)
-        expect(order.get("name")).to(equal(MockOrderForm.name.data))  # type: ignore
-        expect(order.get("address")).to(equal(MockOrderForm.address.data))  # type: ignore
+        with patch("app.GetOrderForm", MockGetOrderForm, create=True):
+            response = self.client.get("/get")
+            order_xml = response.get_data(as_text=True)
+            root = ET.fromstring(order_xml)
+        expect(root.find("name").text).to(equal(MockCreateOrderForm.name.data))  # type: ignore
+        expect(root.find("address").text).to(equal(MockCreateOrderForm.address.data))  # type: ignore
